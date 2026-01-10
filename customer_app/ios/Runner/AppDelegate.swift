f03ba84365d095e03df17bef06637f34f6ccc16a
whoamipolatt <tahahailoupolat@gmail.com>
2026-01-10 21:01:29 +0300

    dfxfvgbhnjmkölç

--- customer_app/ios/Runner/AppDelegate.swift
+++ customer_app/ios/Runner/AppDelegate.swift
@@ -1,6 +1,8 @@
 import Flutter
 import UIKit
 import GoogleMaps
+// [NEW] Import Firebase Messaging
+import FirebaseMessaging
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -11,5 +13,11 @@ import GoogleMaps
     GMSServices.provideAPIKey("AIzaSyBiFIf0b9O4EM7khvcK6Z6I25vfmwzv3Vw")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  // [NEW] Explicitly forward APNs token to Firebase Messaging
+  override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+    Messaging.messaging().apnsToken = deviceToken
+    super.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
   }
 }
